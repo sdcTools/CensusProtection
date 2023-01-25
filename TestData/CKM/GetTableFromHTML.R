4ae3f27ef92fe2c3ab016ddb5a94ca58e1147b47
Peter-Paul de Wolf <p.p.de.wolf@freeler.nl>
2023-01-25 13:57:55 +0100

    Delete TestData directory

--- TestData/CKM/GetTableFromHTML.R
+++
@@ -1,5 +0,0 @@
-library(rvest)
-setwd("E:\\W7\\Users\\Peter-Paul\\MyDocuments\\Thuiswerk\\Cursus\\ESTP\\2021\\CensusSDC\\CKM")
-
-html <- read_html("out\\test_data_10k_tau_out.html")
-html_table(html_nodes(html,"table")[[7]], header=TRUE, dec=",")
